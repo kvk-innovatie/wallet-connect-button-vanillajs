09316131e56b725568f00a9f4cff320f49ca3cfd
Michiel Mayer <michiel.mayer@kvk.nl>
2025-12-12 12:36:16 +0100

    Release v1.0.14

--- src/nl-wallet-web.js
+++ src/nl-wallet-web.js
@@ -5283,8 +5283,8 @@ const {
   return (s) => t[s];
 }, pu = {
   en: {
-    wallet_button_text: "Login with NL Wallet",
-    modal_header: "NL Wallet",
+    wallet_button_text: "Login with Wallet",
+    modal_header: "Wallet",
     need_help: "Need help?",
     stop: "Stop",
     retry: "Try again",
@@ -5293,7 +5293,7 @@ const {
     no: "No",
     confirm_stop_title: "Are you sure you want to stop?",
     confirm_stop_body: "If you stop now, no data will be shared.",
-    device_choice_title: "On which device is your NL Wallet app installed?",
+    device_choice_title: "On which device is your Wallet app installed?",
     device_choice_same_device: "On this device",
     device_choice_cross_device: "On another device",
     expired_title: "Sorry, time is over",
@@ -5304,19 +5304,19 @@ const {
     cancelled_body: "Because you have stopped, no data has been shared.",
     network_title: "Sorry, no internet connection",
     network_body: "Your internet connection seems to be down or too slow. Check your connection and try again.",
-    help_title: "No NL Wallet App yet? Or need help?",
-    help_to_website: "To NL Wallet website",
-    in_progress_title: "Follow the steps in your NL Wallet app",
+    help_title: "No Wallet App yet? Or need help?",
+    help_to_website: "To Wallet website",
+    in_progress_title: "Follow the steps in your Wallet app",
     loading_title: "Please wait",
     loading_body: "Your request is being retrieved",
-    qr_code_title: "Scan the QR code with your NL Wallet app",
+    qr_code_title: "Scan the QR code with your Wallet app",
     qr_code_label: "QR code",
     success_title: "Success!",
     success_body: "Close this page and continue in the new opened tab."
   },
   nl: {
-    wallet_button_text: "Inloggen met NL Wallet",
-    modal_header: "NL Wallet",
+    wallet_button_text: "Inloggen met Wallet",
+    modal_header: "Wallet",
     need_help: "Hulp nodig?",
     stop: "Stoppen",
     retry: "Probeer opnieuw",
@@ -5325,7 +5325,7 @@ const {
     no: "Nee",
     confirm_stop_title: "Weet je zeker dat je wilt stoppen?",
     confirm_stop_body: "Als je stopt worden er geen gegevens gedeeld.",
-    device_choice_title: "Op welk apparaat staat je NL Wallet app?",
+    device_choice_title: "Op welk apparaat staat je Wallet app?",
     device_choice_same_device: "Op dit apparaat",
     device_choice_cross_device: "Op een ander apparaat",
     expired_title: "Sorry, de tijd is voorbij",
@@ -5336,12 +5336,12 @@ const {
     cancelled_body: "Omdat je bent gestopt zijn er geen gegevens gedeeld.",
     network_title: "Sorry, geen internet",
     network_body: "Je verbinding met het internet lijkt niet te werken of is te traag. Controleer je verbinding en probeer het opnieuw.",
-    help_title: "Nog geen NL Wallet app? Of hulp nodig?",
-    help_to_website: "Naar NL Wallet website",
-    in_progress_title: "Volg de stappen in de NL Wallet app",
+    help_title: "Nog geen Wallet app? Of hulp nodig?",
+    help_to_website: "Naar Wallet website",
+    in_progress_title: "Volg de stappen in de Wallet app",
     loading_title: "Even geduld",
     loading_body: "De gegevens worden opgehaald",
-    qr_code_title: "Scan de QR-code met je NL Wallet app",
+    qr_code_title: "Scan de QR-code met je Wallet app",
     qr_code_label: "QR code",
     success_title: "Gelukt!",
     success_body: "Sluit deze pagina en ga verder in het nieuw geopende tabblad."
@@ -6900,7 +6900,7 @@ const tf = /* @__PURE__ */ ai(qu, [["render", ef]]), sf = {
       x("aside", {
         "aria-modal": "true",
         role: "dialog",
-        "aria-label": "NL Wallet",
+        "aria-label": "Wallet",
         class: ut(["modal", [i.value.kind, i.value.kind === "success" && i.value.session.sessionType]]),
         "data-testid": "wallet_modal"
       }, [
@@ -6955,7 +6955,7 @@ const tf = /* @__PURE__ */ ai(qu, [["render", ef]]), sf = {
       x("aside", {
         "aria-modal": "true",
         role: "dialog",
-        "aria-label": "NL Wallet",
+        "aria-label": "Wallet",
         class: ut(["modal", [o.value.kind, o.value.kind === "success" && o.value.session.sessionType]]),
         "data-testid": "wallet_modal"
       }, [
